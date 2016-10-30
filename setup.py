9df3feea998012d1c5180523cabeaaf8d3cc61e1
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2016-10-30 18:13:49 +0000

    Add setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='EyeAreSeeStats',
+      version='0.1',
+      description='A tool for collecting stats from IRC logs',
+      author='Ankur Sinha',
+      author_email='sanjay.ankur AT gmail.com',
+      license='GPLv3',
+      url='https://github.com/sanjayankur31/eyeareseestats',
+      packages=['eyeareseestats', 'eyeareseestats.test']
+      )
+
