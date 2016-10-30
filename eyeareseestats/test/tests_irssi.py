41aa3bce136f45d939ea68f6faba0cc54bc342af
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2016-10-30 09:33:13 +0000

    Add better unittests for IRSSI case

--- eyeareseestats/test/tests_irssi.py
+++ eyeareseestats/test/tests_irssi.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
-Test irssi logs
+Test irssi logs.
 
-File:
+File: tests_irssi.py
 
 Copyright 2016 Ankur Sinha
 Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
@@ -23,37 +23,88 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import unittest
 import pyparsing as pp
-from eyeareseestats.parsers.irssi import logentry
+from eyeareseestats.parsers.irssi import action, notification, nick, chat
 
 
 class TestIRSSILogs(unittest.TestCase):
 
     """Test for IRSSI parser."""
 
-    def test_parser(self):
-        """Match with test strings."""
-        fullstatements = [
-            "21:51 <FranciscoD|Uni> seeing if I can easily extend pisg",
-            "20:40 -!- satya4ever [~sbulage@103.25.3.2] has quit [Ping timeout: 258 seconds]",
-            "14:20 -!- chillsuk [~ch@host-92-18-149-93.as13285.net] has joined #fedora",
-            "22:11  * FranciscoD|Uni goes off for a bit",
-            "21:38 -!- jhunt|kcs is now known as jhunt"
-        ]
-
+    def test_nicks(self):
+        """Test the nicks regex."""
         nicks = [
             'FranciscoD_',
             'FranciscoD|Uni',
             'FranciscoD^|Uni',
-            'FranciscoD\$Uni',
             'FranciscoD-_Uni',
         ]
 
-        for sentence in fullstatements:
+        for entry in nicks:
             try:
-                result = logentry.parseString(sentence)
-                print("Matches: {}".format(result))
+                result = nick.parseString(entry)
             except pp.ParseException as x:
-                print("{} does not match".format(sentence))
+                pass
+
+            self.assertEqual(entry, result.nick)
+
+    def test_actions(self):
+        """Test actions."""
+        example = "22:11  * FranciscoD|Uni goes off for a bit"
+        try:
+            result = action.parseString(example)
+        except pp.ParseException as x:
+            pass
+
+        self.assertEqual("FranciscoD|Uni", result.nick)
+        self.assertEqual("22:11", result.time)
+
+    def test_chats(self):
+        """Test chats."""
+        example = "21:51 <FranciscoD|Uni> seeing if I can easily extend pisg"
+        try:
+            result = chat.parseString(example)
+        except pp.ParseException as x:
+            pass
+
+        self.assertEqual("FranciscoD|Uni", result.nick)
+        self.assertEqual("21:51", result.time)
+
+    def test_joins(self):
+        """Test joins."""
+        example = "14:20 -!- FranciscoD [franciscod@something] has joined #fedora"
+        try:
+            result = notification.parseString(example)
+        except pp.ParseException as x:
+            pass
+
+        self.assertEqual("FranciscoD", result.nick)
+        self.assertEqual("14:20", result.time)
+        self.assertTrue('has joined' in result.detail)
+
+    def test_leaves(self):
+        """Test leaves and quits."""
+        example = "20:40 -!- FranciscoD_ [franciscod@something] has quit [Ping timeout: 258 seconds]"
+        try:
+            result = notification.parseString(example)
+        except pp.ParseException as x:
+            pass
+
+        self.assertEqual("FranciscoD_", result.nick)
+        self.assertEqual("20:40", result.time)
+        self.assertTrue('has quit' in result.detail or 'has left' in result.detail)
+
+    def test_renicks(self):
+        """Test nick changes."""
+        example = "21:38 -!- FranciscoD|Uni is now known as FranciscoD"
+        try:
+            result = notification.parseString(example)
+        except pp.ParseException as x:
+            pass
+
+        self.assertEqual("FranciscoD|Uni", result.nick)
+        self.assertEqual("21:38", result.time)
+        self.assertTrue('is now known as' in result.detail)
+
 
 if __name__ == "__main__":
     unittest.main()
